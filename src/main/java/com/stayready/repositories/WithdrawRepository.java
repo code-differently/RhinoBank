ce63b502cab0ab32ec79a80489fd36e5085b8c56
Solomon Jackson <solomonjackson@solomons-air.myfiosgateway.com>
2020-08-10 18:18:28 -0400

    Added Deposit and Withdraw Repositories

---
+++ src/main/java/com/stayready/repositories/WithdrawRepository.java
@@ -0,0 +1,8 @@
+package com.stayready.repositories;
+
+import com.stayready.domain.Withdraw;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WithdrawRepository extends CrudRepository<Withdraw, Long> {
+
+}
