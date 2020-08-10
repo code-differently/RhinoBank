e01ef756b913db0357876bb977c99e6281c1ec55
Solomon Jackson <solomonjackson@solomons-air.myfiosgateway.com>
2020-08-10 18:15:36 -0400

    Added Spring annotations to Deposit and Withdraw.

--- src/main/java/com/stayready/domain/Withdraw.java
+++ src/main/java/com/stayready/domain/Withdraw.java
@@ -1,14 +1,37 @@
 package com.stayready.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Withdraw {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "DEPOSIT_ID")
     private Long id;
+
+    @Column(name = "Deposit_TYPE")
     private String type;
+
+    @Column(name = "TRANSACTION_DATE")
     private String transaction_date;
+
+    @Column(name = "STATUS")
     private String status;
+
+    @Column(name = "PAYEE_ID")
     private Long payee_id;
+
+    @Column(name = "MEDIUM")
     private String medium;
+
+    @Column(name = "AMOUNT")
     private Double amount;
+
+    @Column(name = "DESCRIPTION")
     private String description;
 
     public Long getId() {
